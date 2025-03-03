12f025ddcdd973203f18d66900e7d3513e3ea018
lxxzdrgnl <pung4905@naver.com>
2025-03-03 18:37:10 +0900

    upload

--- Prior/2493_top.cpp
+++ Prior/2493_top.cpp
@@ -2,40 +2,32 @@
 #include <bits/stdc++.h>
  
 using namespace std;
- 
-vector<int> findTower(int n,vector<int>& heights) {
-    vector<int> result(n,0);
-    stack<pair<int, int>> s;
 
-    for (int i=0; i<n; i++){
-        while (!s.empty() && s.top().second < heights[i]){
-            s.pop();
-        }
-        if (!s.empty()){
-            result[i] = s.top().first + 1;
-        }
-
-        s.push({i,heights[i]});
-    }
-    return result;
-}
-
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int n;
+ 
+    stack<pair<int, int> > s;  // index, height
+    int n, height;
     cin >> n;
-
-    vector<int> heights(n);
-    for (int i = 0; i<n; i++){
-        cin >> heights[i];
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> height;
+ 
+        while (!s.empty()) {
+            if (height < s.top().second) {
+                cout << s.top().first << " ";
+                break;
+            }
+            //수신탑 찾을 때까지 계속 pop
+            s.pop();
+        }
+        if (s.empty()) {
+            cout << 0 << " ";
+        }
+        //현재 높이를 스택에 푸쉬
+        s.push(make_pair(i + 1, height));
     }
-
-    vector<int> result = findTower(n,heights);
-
-    for (int i=0; i<n; i++){
-        cout << result[i] << " ";
-    }
+     
+    return 0;
 }
