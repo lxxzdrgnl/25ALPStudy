10ed0ef4b5a4bf248ba0a77060a9ff99b69d8d09
lxxzdrgnl <pung4905@naver.com>
2025-03-06 20:17:09 +0900

    upload

--- Prior/1967_treeR.cpp
+++ Prior/1967_treeR.cpp
@@ -4,7 +4,43 @@
 
 using namespace std;
 
+vector<pair<int, int>> tree[10001];
+bool visited[10001];
+int maxDist = 0, farthestNode = 0;
+
+void dfs(int node, int dist) {
+    visited[node] = true;
+
+    if (dist > maxDist) {
+        maxDist = dist;
+        farthestNode = node;
+    }
+
+    for (const auto& edge : tree[node]) {
+        if (!visited[edge.first]) {               // 첫 번째 노드
+            dfs(edge.first, dist + edge.second);  // 두 번째  가중치
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        tree[u].push_back({v, w});
+        tree[v].push_back({u, w});
+    }
+
+    dfs(1, 0);
+    fill(begin(visited), end(visited), false);  // 방문 배열 초기화
+
+    dfs(farthestNode, 0);  // 가장 먼 노드에서 DFS
+
+    cout << maxDist << endl;
 }
